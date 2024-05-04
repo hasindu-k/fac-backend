c4009238054eaa43c6e23098e579ce86cdd31c0d
thenuri02 <thenurimalaviarachchi123@gmail.com>
2024-05-04 18:02:31 +0530

    added backend files
    Uruwela Tea Factory-delivery management system

---
+++ Models/DeliveryModel.cs
@@ -0,0 +1,17 @@
+﻿namespace teaFactory.Models
+{
+    public class DeliveryModel
+    {
+        public int deliveryId { get; set; }
+        public DateTime deliveryDate { get; set; }
+        public string driverId { get; set; }
+
+        public string CusName { get; set; }
+        public string CusContact { get; set; }
+        public string Cusaddress { get; set; }
+        public string delStatus { get; set; }
+
+
+
+    }
+}
---
+++ Models/DriverModel.cs
@@ -0,0 +1,14 @@
+﻿namespace teaFactory.Models
+{
+    public class DriverModel
+    {
+        public string driverId { get; set; }
+        public string driverName { get; set; }
+        public string driverContact { get; set; }
+        public string driverEmail { get; set; }
+        public string driverAddress { get; set; }
+        public double driverSal { get; set; }
+        public string diverLic { get; set; }
+
+    }
+}
